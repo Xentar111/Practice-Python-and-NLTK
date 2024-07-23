24d6ec2d591f5d0c6b59869dbbfd97aa8780106f
[Carlos Matias] <xentar111@gmail.com>
2024-07-23 18:07:04 -0300

    tokenized text

--- add_news_docs.py
+++ add_news_docs.py
@@ -1,5 +1,6 @@
 import os
 import codecs
+import nltk
 
 docs = './docs'
 
@@ -9,4 +10,7 @@ for root, folder_docs, files_docs in os.walk(docs, topdown=False):
         f = codecs.open(os.path.join(root, doc), 'r', 'utf-8')
         text = f.read()
         f.close
-        print(text)
+
+words = nltk.word_tokenize(text)
+#print(text)
+print(words)
